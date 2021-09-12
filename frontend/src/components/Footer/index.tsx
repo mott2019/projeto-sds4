1e317eadd5d83e1f2099d17ae06bfb87b9dc645c
André Mott <vendasobambu@gmail.com>
2021-09-12 00:44:14 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+
+function Footer() {
+    return (
+
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/mott2019/projeto-sds4" target="_blank" rel="noreferrer">André Mott Fernandez</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
+
+
